347fe642a3440f9d75782802981f9a5783c6c2c9
Paul De Silos <pauldesilos@yahoo.com>
2025-01-20 00:29:22 -0600

    edited config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/sweng861-hw1-2",
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
